09e102f033e714c3bf2f6d03c4fde7ee595cfb3b
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-25 01:41:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,25 @@
+# while as the Iterator!!!
+# take an array of numbers
+# and square each number 
+# return a ***new*** array with squared numbers 
+
+
+#While(conditional) iterator 
+ #loop
+ #while <<<condition>>> do 
+ ###execution code 
+ # end 
+
+
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+while counter < array.length do
+  counter += 1
+  #counter = counter + 1
+  new_array << array[counter] ** 2
+  
 end
+#return the new array
+new_array
+end 
