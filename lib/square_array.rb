f286e67fee869c650fdaea8ac6c9d5c2b507051f
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-25 01:53:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,8 @@ def square_array(array)
   counter = 0
 while array[counter] do
   counter += 1
-  #counter = counter + 1
   new_array << array[counter] ** 2
   
 end
-# return the new array
 new_array
 end 
